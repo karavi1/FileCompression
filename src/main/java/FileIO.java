cc45688611437f2c8f9044ee53e44ade99ca9e4c
Kaushik Ravi <kaush.ravi@gmail.com>
2019-06-16 15:37:09 -0700

    Created FileIO and Huffman

--- src/main/java/FileIO.java
+++ src/main/java/FileIO.java
@@ -1,2 +1,19 @@
+
+import java.io.*;
+
 public class FileIO {
+
+    private File file;
+    private byte[] byteFile;
+
+    public FileIO(File file){
+
+        this.file = file;
+
+    }
+
+    public byte[] fileToBytes(){
+//        FileInputStream fis = new FileInputStream(file);
+        return null;
+    }
 }
