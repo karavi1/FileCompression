b39c599e4517753d339ff4a5b55b274981c34710
Kaushik Ravi <kaush.ravi@gmail.com>
2019-06-17 11:41:48 -0700

    learning test directories in gradle

---
+++ src/test/java/FileIOTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import java.nio.channels.FileLock;
+
+public class FileIOTest {
+
+
+    public FileIOTest(){
+
+    }
+
+    @Test
+    public void testReadBytes()
+    {
+//        File file = new File(null);
+//        FileIO fio = new FileIO();
+        assert 1 == 1;
+    }
+}
