a5d800bfe9c4dc509d10ddca02f95f1172c2343d
Kaushik Ravi <kaushik.ravi@marklogic.com>
2019-06-18 10:57:04 -0700

    set up huffman, basic functionality, haven't written algorithm yet

--- src/main/java/Huffman.java
+++ src/main/java/Huffman.java
@@ -1,23 +1,73 @@
 
 import java.io.*;
-import java.nio.*;
 import java.util.*;
 
 public class Huffman {
 
-    Map<Character, Integer> frequencies;
+    private Map<Character, Integer> frequencies;
+    private FileMetadata fm;
+    private byte[] bytes;
 
     public Huffman(){
 
-        frequencies = new HashMap<>();
+        this.frequencies = new HashMap<>();
         File file = new File("abcdef.txt");
-        FileMetadata fm = new FileMetadata(file);
+        this.fm = new FileMetadata(file);
+        this.bytes = fm.getBytes();
 
     }
 
+    public void buildFrequencies(){
+
+    }
+
+    public void buildMinHeap(){
+
+    }
+
+    public void buildHuffmanTree(){
+
+    }
+
+    public void buildEncodings(){
+
+    }
+
+    // Getters
+    public Map<Character, Integer> getFrequencies(){
+        return this.frequencies;
+    }
+
+    public FileMetadata getFileMetadata(){
+        return this.fm;
+    }
+
+    public byte[] getFileBytes(){
+        return this.bytes;
+    }
+
+    public File getFile(){
+        return this.fm.getFile();
+    }
+
+
+    // Setters
+    public void setFrequencies(Map<Character, Integer> newFrequencies){
+        this.frequencies = newFrequencies;
+    }
+
+    public void setFileMetadata(FileMetadata newFm){
+        this.fm = newFm;
+    }
+
+    public void setFileBytes(byte[] newBytes){
+        this.bytes = newBytes;
+    }
+
+
+    // Main Method (temporary before writing tests)
     public static void main (String [] args){
         Huffman h = new Huffman();
-
     }
 
 }
